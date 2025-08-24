239707df5ea6dc8c23b4241fe44f949f0130eca9
elbek <elbek4561@gmail.com>
2025-08-24 18:54:08 +0500

    telegram id

--- component/ModalPartners.js
+++ component/ModalPartners.js
@@ -27,8 +27,8 @@ export default function ModalPartners({ setOpenPartnersModal }) {
     }
   }
 
-  const token = "5898057424:AAHPSH1xom0rFNHCHYN_9MgVJzdizDctelA";
-  const chad_id = -1001942371012;
+  const token = "8036896960:AAFyZU4HVrUwkJFPT--zND1qKidtwSmiwlo";
+  const chad_id = 8226799639;
 
   const handleHCaptchaVerify = (responseToken) => {
     setHCaptchaResponse(responseToken);
